664e6b8310c1cf9200fb345f87090aee028f9b5a
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-04-06 17:28:23 +0300

    initialize_database_test.py

---
+++ harjoitustyo/src/tests/initialize_database_test.py
@@ -0,0 +1,13 @@
+import unittest
+from initialize_database import initialize_database
+from file_reader import Repository
+from database_connection import get_database_connection
+
+class TestInitialize_database(unittest.TestCase):
+    def setUp(self):
+        self.file_reader = Repository(get_database_connection())
+
+    def test_initialize_database(self):
+        self.file_reader.add_expense(100, 'test')
+        initialize_database()
+        self.assertEqual(self.file_reader.get_data_expenses(), [])
